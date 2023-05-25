9d472dd6d9416460d7bc700d1fc18b5e0149a542
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-05-25 11:03:36 -0700

    updates for lab 5

--- src/components/About.js
+++ src/components/About.js
@@ -28,7 +28,7 @@ class About extends Component {
                   <img
                     height="250px"
                     src={profilepic}
-                    alt="Avatar placeholder"
+                    alt="profile pic"
                   />
                   <Icon
                     icon={angularIcon}
